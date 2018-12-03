e643fe98dc45661464d22ef68d606a25e8a8d0e9
Tract <tzlang@stanford.edu>
2018-12-02 19:21:32 -0800

    pac4_version

---
+++ Assets/MazeGenerator/Scripts/RollerBall.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//<summary>
+//Ball movement controlls and simple third-person-style camera
+//</summary>
+public class RollerBall : MonoBehaviour {
+
+	public GameObject ViewCamera = null;
+	public AudioClip JumpSound = null;
+	public AudioClip HitSound = null;
+	public AudioClip CoinSound = null;
+
+	private Rigidbody mRigidBody = null;
+	private AudioSource mAudioSource = null;
+	private bool mFloorTouched = false;
+
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector3 moveDir;
+
+	void Start () {
+		mRigidBody = GetComponent<Rigidbody> ();
+		mAudioSource = GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate () {
+		if (mRigidBody != null) {
+			if (Input.GetButton ("Horizontal")) {
+				mRigidBody.AddTorque(Vector3.back * Input.GetAxis("Horizontal")*10);
+			}
+			if (Input.GetButton ("Vertical")) {
+				mRigidBody.AddTorque(Vector3.right * Input.GetAxis("Vertical")*10);
+			}
+			if (Input.GetButtonDown("Jump")) {
+				if(mAudioSource != null && JumpSound != null){
+					mAudioSource.PlayOneShot(JumpSound);
+				}
+				mRigidBody.AddForce(Vector3.up*200);
+			}
+		}
+        if(OVRInput.GetDown(OVRInput.Button.DpadRight))
+        {
+            transform.Rotate(new Vector3(0, 90, 0));
+        }
+        if (OVRInput.GetDown(OVRInput.Button.DpadLeft))
+        {
+            transform.Rotate(new Vector3(0, -90, 0));
+        }
+        if (OVRInput.Get(OVRInput.Button.DpadDown))
+        {
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+
+        //VRInput.Touch.PrimaryTouchpad;
+        Vector2 touch = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+
+        transform.Translate(moveDir * speed * Time.deltaTime);
+        //transform.Translate(new Vector3(0, 0, ));
+       // transform.position.z += 1;
+
+        if (ViewCamera != null) {
+			Vector3 direction = (Vector3.up*2+Vector3.back)*2;
+			RaycastHit hit;
+			Debug.DrawLine(transform.position,transform.position+direction,Color.red);
+			if(Physics.Linecast(transform.position,transform.position+direction,out hit)){
+				ViewCamera.transform.position = hit.point;
+			}else{
+				ViewCamera.transform.position = transform.position+direction;
+			}
+			ViewCamera.transform.LookAt(transform.position);
+		}
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = true;
+			if (mAudioSource != null && HitSound != null && coll.relativeVelocity.y > .5f) {
+				mAudioSource.PlayOneShot (HitSound, coll.relativeVelocity.magnitude);
+			}
+		} else {
+			if (mAudioSource != null && HitSound != null && coll.relativeVelocity.magnitude > 2f) {
+				mAudioSource.PlayOneShot (HitSound, coll.relativeVelocity.magnitude);
+			}
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			if(mAudioSource != null && CoinSound != null){
+				mAudioSource.PlayOneShot(CoinSound);
+			}
+			Destroy(other.gameObject);
+		}
+	}
+}
